b30d898a3939d9e79d399f3159fe3f1409bace70
justinjavelin <112462844+justinjavelin@users.noreply.github.com>
2026-09-04 17:40:39 -0700

    Update test_score_average.py
    Added f-string to round output float to 2 decimal places

--- test_score_average.py
+++ test_score_average.py
@@ -2,6 +2,8 @@
 # function, with user input as a float value instead of an integer.
 # Get three test scores and assign them to the
 # test1, test2, and test3 variables.
+# For a simpler output, an f-string is used with .2 as a precision
+# designator to round repeating decimal float values to two decimal places.
 
 test1 = float(input('Enter the first test score: '))
 test2 = float(input('Enter the second test score: '))
@@ -14,4 +16,4 @@ average = (test1 + test2 + test3) / 3.0
 
 # Display the average.
 
-print('The average score is', average)
+print(f'The average score is {average: .2f}.')
